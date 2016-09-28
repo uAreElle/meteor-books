d537dd1da40969a01bdee7c2fb66f3e2a252446a
Michelle Nguyen <mnguye96@ucsc.edu>
2016-09-28 02:23:27 -0700

    test 2nd commit

--- imports/api/books/books.js
+++ imports/api/books/books.js
@@ -1,1 +1,21 @@
 // /imports/api/books/books.js
+
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+// Defining server side collection 
+
+Books = new Mongo.Collection('Books');
+
+// Defining schema 
+
+Books.schema = new SimpleSchema({
+	title: { type: String },
+	author: { type: String },
+	genre: { type: String },
+	year: { type: Number },
+})
+
+// Attaching schema 
+
+Books.attachSchema(Books.schema);
